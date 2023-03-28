679354214e74055a556ab965935a9486406bc460
MichalWn <michalw95@gmail.com>
2023-03-28 10:54:47 +0200

    Add css

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,12 +1,30 @@
 <template>
+  <div>
     <h1>{{ this.tittle }}</h1>
+  </div>
 </template>
 
 <script>
   export default{
     data() {
         return {
-            tittle: 'Recipes works' 
+            tittle: 'Recipes works'
         }
     }}
 </script>
+
+<style lang="scss" scoped >
+
+// a.active-link{
+//   color: black  !important;
+// }
+
+div {
+  display: grid;
+  margin: 8%;
+  justify-content: center;
+  align-items: center;
+
+}
+
+</style>
