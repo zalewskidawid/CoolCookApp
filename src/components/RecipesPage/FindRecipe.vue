b67557475d5fdb919041da99c4e13b60f112fdfa
Dawid Wymoczylo <100722038+L4kier@users.noreply.github.com>
2023-05-26 14:20:50 +0200

    Update FindRecipe.vue

--- src/components/RecipesPage/FindRecipe.vue
+++ src/components/RecipesPage/FindRecipe.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="information"></div>
   <div id="recipes">
-    <v-btn @click="underlineIngredients">Show selected ingredients</v-btn>
-  <div v-for="recipe in filterdRecipes" :key="recipe.id">
-    <Recipe :title="recipe.title" :ingredients="recipe.ingredients" :steps="recipe.steps" :categories="recipe.categories" :description="recipe.description" :id="recipe.id">
-    </Recipe>
+    <div v-for="recipe in filterdRecipes" :key="recipe.id">
+      <Recipe
+        :title="recipe.title"
+        :ingredients="recipe.ingredients"
+        :steps="recipe.steps"
+        :categories="recipe.categories"
+        :description="recipe.description"
+        :id="recipe.id"
+      >
+      </Recipe>
+    </div>
   </div>
-</div>
-  <div id="ingredients">
-
-  </div>
+  <div id="ingredients"></div>
   <v-btn @click="showIngredientPopup">Add ingredient</v-btn>
   <v-btn @click="findRecipes">Find recipe</v-btn>
   <BasePopup :base-dialog="baseDialog" @close-popup="closeIngredientsPopup">
-    <v-text-field @input="filterIngredients" v-model="searchIngredient" label="Search Ingredient"
-      placeholder="Ingredient name"></v-text-field>
+    <v-text-field
+      @input="filterIngredients"
+      v-model="searchIngredient"
+      label="Search Ingredient"
+      placeholder="Ingredient name"
+    ></v-text-field>
     <div class="ingredients-search-wrapper">
       <p v-if="!filteredArray">No ingredients found.</p>
-      <div v-for="(ingredient, index) in filteredArray" :key="index" class="my-2">
-        <p @click="addIngredientToInput(ingredient)" class="pb-2 ingredient-search-name">{{ ingredient }}</p>
+      <div
+        v-for="(ingredient, index) in filteredArray"
+        :key="index"
+        class="my-2"
+      >
+        <p
+          @click="addIngredientToInput(ingredient)"
+          class="pb-2 ingredient-search-name"
+        >
+          {{ ingredient }}
+        </p>
         <v-divider></v-divider>
       </div>
     </div>
@@ -40,39 +57,37 @@ export default {
       filteredArray: this.databaseIngredients,
       databaseIngredients: [],
       selectedIngredients: [],
-
-    }
+    };
   },
   methods: {
     closeIngredientsPopup() {
       this.baseDialog = false;
-      this.searchIngredient = '';
+      this.searchIngredient = "";
       this.filteredArray = this.databaseIngredients;
     },
     addIngredientToInput(ingredient) {
-      const ingredients = document.querySelector('#ingredients');
-      const div = document.createElement('div');
-      div.style='text-align: center; width: auto; padding: 10px;border-radius: 20px;background-color: pink;font-weight: 700;margin: 5px; border: solid 2px black;';
-      const p = document.createElement('p');
+      const ingredients = document.querySelector("#ingredients");
+      const div = document.createElement("div");
+      div.style =
+        "text-align: center; width: auto; padding: 10px;border-radius: 20px;background-color: pink;font-weight: 700;margin: 5px; border: solid 2px black;";
+      const p = document.createElement("p");
       div.appendChild(p);
-      this.searchIngredient = '';
+      this.searchIngredient = "";
       this.filteredArray = this.databaseIngredients;
       const childArray = [];
-      ingredients.childNodes.forEach(element => {
+      ingredients.childNodes.forEach((element) => {
         childArray.push(element.innerText);
-      })
+      });
 
       if (!childArray.includes(ingredient)) {
         p.innerText = ingredient;
         ingredients.appendChild(div);
 
-        console.log(this.databaseIngredients)
         this.databaseIngredients.forEach((el, index) => {
           if (this.databaseIngredients[index] === ingredient) {
             this.databaseIngredients.splice(index, 1);
           }
-          console.log(this.databaseIngredients)
-        })
+        });
       }
       this.baseDialog = false;
     },
@@ -81,30 +96,22 @@ export default {
     },
     filterIngredients() {
       this.filteredArray = [];
-      this.databaseIngredients.filter(el => {
+      this.databaseIngredients.filter((el) => {
         if (el.toString().startsWith(this.searchIngredient)) {
           this.filteredArray.push(el);
         }
-      })
+      });
     },
     async reloadIngredients() {
       await this.getIngredientsFromDatabase();
       this.filteredArray = this.getIngredientsFromDatabase();
-
     },
     findRecipes() {
-      const ingredients = document.querySelector('#ingredients');
+      const ingredients = document.querySelector("#ingredients");
       const selectedIngredients = [];
-      ingredients.childNodes.forEach(element => selectedIngredients.push(element.innerText))
-      console.log(selectedIngredients);
-
-      const testArray = ['olive oil', 'coca cola', 'tomatoes'];
-      const isFounded = testArray.some(element => selectedIngredients.includes(element));
-
-      if (isFounded) {
-        console.log(`Recipe founded with ingredients: ${testArray}`)
-
-      }
+      ingredients.childNodes.forEach((element) =>
+        selectedIngredients.push(element.innerText)
+      );
 
       this.recipes = [];
       this.formattedRecipes = [];
@@ -112,28 +119,33 @@ export default {
       this.reloadIngredients();
 
       this.getRecipesFromDatabase(selectedIngredients);
-      ingredients.innerText = '';
+      ingredients.innerText = "";
       this.selectedIngredients = [];
       this.selectedIngredients = selectedIngredients;
 
-
+      this.underlineIngredients();
     },
-     underlineIngredients(){
-      const ingredientsElements = document.querySelectorAll('.ingredients');
-      ingredientsElements.forEach(element=>{
-        let previousText = element.innerText;
-        let newText = previousText;
-        this.selectedIngredients.forEach(ingredient=>{
-          
-          if(previousText.includes(ingredient)){
-            previousText=newText;
-            newText = previousText.replace(ingredient,`<span>${ingredient}</span>`); 
-          }else{
-            //do nothing
-          }
-        })
-        element.innerHTML = newText;
-      })
+
+    underlineIngredients() {
+      setTimeout(() => {
+        const ingredientsElements = document.querySelectorAll(".ingredients");
+        ingredientsElements.forEach((element) => {
+          let previousText = element.innerText;
+          let newText = previousText;
+          this.selectedIngredients.forEach((ingredient) => {
+            if (previousText.includes(ingredient)) {
+              previousText = newText;
+              newText = previousText.replace(
+                ingredient,
+                `<span style="background-color:pink; border-radius:10px; padding:5px; border: solid 1px black; font-size: 2rem; font-weight: 700;">${ingredient}</span>`
+              );
+            } else {
+              //do nothing
+            }
+          });
+          element.innerHTML = newText;
+        });
+      }, 100);
     },
 
     async getRecipesFromDatabase(selectedIngredients) {
@@ -145,67 +157,62 @@ export default {
         const values = Object.entries(parsedData);
 
         values.forEach((element) => {
-          let ingredientsString = '';
-          let categoriesString = '';
-          let stepsArray = '';
-          element[1]['ingredients'].forEach(function(el, index) {
-            if(index === element[1]['ingredients'].length - 1) {
+          let ingredientsString = "";
+          let categoriesString = "";
+          let stepsArray = "";
+          element[1]["ingredients"].forEach(function (el, index) {
+            if (index === element[1]["ingredients"].length - 1) {
               ingredientsString = ingredientsString + el.name;
             } else {
-              ingredientsString = ingredientsString + el.name + ', ';
+              ingredientsString = ingredientsString + el.name + ", ";
             }
-          })
-          element[1]['categories'].forEach(function(el, index) {
-            if(index === element[1]['categories'].length - 1) {
+          });
+          element[1]["categories"].forEach(function (el, index) {
+            if (index === element[1]["categories"].length - 1) {
               categoriesString = categoriesString + el;
             } else {
-              categoriesString = categoriesString + el + ', ';
+              categoriesString = categoriesString + el + ", ";
             }
-          })
-          element[1]['steps'].forEach(function(el, index) {
-            if(index === element[1]['steps'].length - 1) {
-              stepsArray = stepsArray + (index+1) + '.' + el;
+          });
+          element[1]["steps"].forEach(function (el, index) {
+            if (index === element[1]["steps"].length - 1) {
+              stepsArray = stepsArray + (index + 1) + "." + el;
             } else {
-              stepsArray = stepsArray + (index+1) + '.' + el + "\n";
+              stepsArray = stepsArray + (index + 1) + "." + el + "\n";
             }
-          })
+          });
           this.formattedRecipes.push({
             id: element[0],
             categories: categoriesString,
-            title: element[1]['title'],
-            description: element[1]['description'],
-            ingredients:  ingredientsString,
-            steps: stepsArray
-        })
+            title: element[1]["title"],
+            description: element[1]["description"],
+            ingredients: ingredientsString,
+            steps: stepsArray,
+          });
 
-        this.recipes.push({ id: element[0], ...element[1] });
-        console.log(this.recipes)
+          this.recipes.push({ id: element[0], ...element[1] });
         });
-        const result = (JSON.parse(JSON.stringify(this.recipes)));
+        const result = JSON.parse(JSON.stringify(this.recipes));
 
         result.forEach((element, index) => {
           const ingredientsArray = [];
-          element.ingredients.forEach(el => {
+          element.ingredients.forEach((el) => {
             ingredientsArray.push(el.name);
-            console.log(ingredientsArray);
-          })
+          });
 
-          const isFounded = ingredientsArray.some(element => selectedIngredients.includes(element));
-          console.log(isFounded);
+          const isFounded = ingredientsArray.some((element) =>
+            selectedIngredients.includes(element)
+          );
 
           if (isFounded) {
-            const information = document.querySelector('#information');
-            console.log(index);
+            const information = document.querySelector("#information");
             this.filterdRecipes.push(this.formattedRecipes[index]);
-            information.innerText = ''
-            console.log(this.filterdRecipes)
+            information.innerText = "";
             return;
           } else if (!isFounded) {
-            information.innerText = 'No Recipes found'
+            information.innerText = "No Recipes found";
           }
-
         });
-
       } catch (err) {
         // dialog.value = true;
         // errorText.value = err.message || "Failed to get recipes";
@@ -217,32 +224,30 @@ export default {
       const array = [];
       this.databaseIngredients = [];
       try {
-        await this.$store.dispatch('ingredientsStore/getIngredientsFromDatabase');
-        this.$store.getters["ingredientsStore/getIngredients"].forEach(el => {
+        await this.$store.dispatch(
+          "ingredientsStore/getIngredientsFromDatabase"
+        );
+        this.$store.getters["ingredientsStore/getIngredients"].forEach((el) => {
           this.databaseIngredients.push(el);
           array.push(el);
 
           return array;
-        })
+        });
       } catch (err) {
         // this.dialog = true;
         // this.errorText = err.message || 'Failed to get ingredients';
       }
-
     },
-
   },
   mounted() {
     this.getIngredientsFromDatabase();
     this.filteredArray = this.databaseIngredients;
   },
-  components: { Recipe, BasePopup }
-
+  components: { Recipe, BasePopup },
 };
 </script>
 
 <style lang="scss" scoped>
-
 #information {
   text-align: center;
   font-weight: 700;
@@ -253,4 +258,3 @@ export default {
   grid-template-columns: 200px 200px 200px;
 }
 </style>
-
